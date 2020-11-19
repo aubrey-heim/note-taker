2ef0e65d2d229d8ca8d3af09463265c068c6e3e3
aubrey-heim <aubrey.l.heim@gmail.com>
2020-11-18 19:10:34 -0800

    updating port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const json = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -68,6 +68,9 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 //sends user to homepage
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
